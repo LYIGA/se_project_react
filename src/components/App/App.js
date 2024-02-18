7222702ed06d4aa1e9fc94c9633bc99372f498ff
SmithAkin <s29smit@gmail.com>
2024-02-18 12:54:16 -0600

    changes to project 10

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,7 +5,18 @@ function App() {
   return (
     <div>
       <header className='header'>
-  
+        <div className='header__logo'>
+          <div>Logo</div>
+          <div>Date</div>
+        </div>
+  <div></div>
+    <div className='header__avatar-logo'>
+      <div>
+    <button type="text" >Add New Clothes</button>
+    </div>
+    <div>Name</div>
+    <div>Avatar Logo</div>
+    </div>
       </header>
     </div>
   );
