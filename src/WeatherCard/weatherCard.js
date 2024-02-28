83e7b94c5e255d9b2141433274e6d1e42d994d63
SmithAkin <s29smit@gmail.com>
2024-02-27 20:49:19 -0600

    update to weatherCard.js

--- src/WeatherCard/weatherCard.js
+++ src/WeatherCard/weatherCard.js
@@ -1,8 +1,8 @@
 const weatherOptions = [
- {url:"/images/day/sunny.svg", day: true, type: "sunny"},
- {url:"/images/day/cloudy.svg", day: true, type: "cloudy"},
- {url:"/images/night/cloud.svg", day: false, type: "cloud"},
- {url:"/images/night/moon.svg", day: false, type: "moon"},
+ {url: require("../images/day/sunny.svg"), day: true, type: "sunny"},
+ {url: require("../images/day/cloudy.svg"), day: true, type: "cloudy"},
+ {url: require("../images/night/cloud.svg"), day: false, type: "cloud"},
+ {url: require("../images/night/moon.svg"), day: false, type: "moon"},
 ];
 
 const WeatherCard = ({day, type}) => {
