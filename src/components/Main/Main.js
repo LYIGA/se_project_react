249d6530734b3a6906ebe93368ad52e769e013b4
SmithAkin <s29smit@gmail.com>
2024-03-10 14:59:10 -0500

    update to main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,7 +4,7 @@ import ItemCard from "../ItemCard/ItemCard";
 
 function Main({weatherTemp}) {
     return <main className='main'>
-      <WeatherCard day={false} type='storm' />
+      <WeatherCard day={false} type='storm' weatherTemp={weatherTemp}/>
       <section className='card_section' id='card section'>
         Today is {weatherTemp} / You may want to wear:
         <div className='card_items'>
