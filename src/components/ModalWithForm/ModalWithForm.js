71267273934d702cfb2d1db3ceeb20db35281d6a
SmithAkin <s29smit@gmail.com>
2024-03-24 07:50:43 -0500

    update to project 10

--- src/components/ModalWithForm/ModalWithForm.js
+++ src/components/ModalWithForm/ModalWithForm.js
@@ -12,7 +12,7 @@ const ModalWithForm = ({
     return (
         <div className={`modal modal_type_${name}`}>
             <div className="modal__content">
-                <button type="button" onClick={onClose}>
+                <button className="modal__name" type="button" onClick={onClose}>
                 Close
                 </button>
                 
