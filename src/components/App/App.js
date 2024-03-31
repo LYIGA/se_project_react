2bf8235f07aa139be2321108521f34373f3c98de
SmithAkin <s29smit@gmail.com>
2024-03-31 06:34:30 -0500

    update to project 10

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,26 +4,26 @@ import Header from "../Header/header.js";
 import Main from "../Main/Main";
 import Footer from "../Footer/footer";
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import ItemModal from "../../ItemModal/ItemModal";
 import { getForcastWeather, parseWeatherData } from "../util/WeatherApi";
 
 function App() {
   const weatherTemp = "30";
-  const [activeModal, setActiveModal] = useState('')
-  const [selectedCard, setSelectedCard] = useState({})
-  const [temp, setTemp] = useState(0)
+  const [activeModal, setActiveModal] = useState("");
+  const [selectedCard, setSelectedCard] = useState({});
+  const [temp, setTemp] = useState(0);
 
   const handleCreateModal = () => {
-    setActiveModal('create')
-  }
-
-  const handleCloseModal = () => {
-    setActiveModal("")
+    setActiveModal("create");
   };
 
-  const handleSelectedCard =(card) => {
-    setActiveModal("preview")
+  const handleCloseModal = () => {
+    setActiveModal("");
+  };
+
+  const handleSelectedCard = (card) => {
+    setActiveModal("preview");
     setSelectedCard(card);
   };
 
@@ -31,47 +31,60 @@ function App() {
     getForcastWeather().then((data) => {
       console.log(data);
       const temperature = parseWeatherData(data);
-      setTemp(temperature)
+      setTemp(temperature);
     });
   });
 
   return (
     <div>
-      <Header  onCreateModal={handleCreateModal}/>
+      <Header onCreateModal={handleCreateModal} />
       <Main weatherTemp={temp} onselectCard={handleSelectedCard} />
       <Footer />
-     {activeModal === 'create' && (
-     <ModalWithForm title="New Garment" onClose={handleCloseModal}>
-        <label className="modal__label">
-          Name
-          <input className="modal__input" placeholder="Name" type="text" name="name" minLength="1" maxLength="30" />
-          </label> 
-          <label className="modal__label">
-          Image
-          <input className="modal__input" placeholder="Image URL" type="text" name="link" minLength="1" maxLength="30" />
+      {activeModal === "create" && (
+        <ModalWithForm title='New Garment' onClose={handleCloseModal}>
+          <label className='modal__label'>
+            Name
+            <input
+              className='modal__input'
+              placeholder='Name'
+              type='text'
+              name='name'
+              minLength='1'
+              maxLength='30'
+            />
           </label>
-          <p className="modal__weather-type">Select the weather type:</p>
-          <div className="modal__radio">
+          <label className='modal__label'>
+            Image
+            <input
+              className='modal__input'
+              placeholder='Image URL'
+              type='url'
+              name='link'
+              minLength='1'
+            />
+          </label>
+          <p className='modal__weather-type'>Select the weather type:</p>
+          <div className='modal__radio'>
             <div>
-              <input type="radio" id="hot" value="hot" />
+              <input type='radio' id='hot' value='hot' />
               <label>Hot</label>
             </div>
             <div>
-              <input type="radio" id="warm" value="warm" />
+              <input type='radio' id='warm' value='warm' />
               <label>Warm</label>
             </div>
             <div>
-              <input type="radio" id="cold" value="cold" />
+              <input type='radio' id='cold' value='cold' />
               <label>Cold</label>
             </div>
-            </div> 
-      </ModalWithForm>
+          </div>
+        </ModalWithForm>
       )}
-      {activeModal === 'preview' && (
+      {activeModal === "preview" && (
         <ItemModal selectedCard={selectedCard} onClose={handleCloseModal} />
       )}
-      </div>
+    </div>
   );
-      }
+}
 
 export default App;
--- src/components/ModalWithForm/ModalWithForm.js
+++ src/components/ModalWithForm/ModalWithForm.js
@@ -1,29 +1,29 @@
-import { useState } from 'react';
-import './ModalWithForm.css'
+import { useState } from "react";
+import "./ModalWithForm.css";
+import closeIcon from "../../images/close1.svg";
 
 const ModalWithForm = ({
-    children, 
-    buttonText = "Add garment",
-     title,
-      onClose, 
-      name,
-     }) => {
+  children,
+  buttonText = "Add garment",
+  title,
+  onClose,
+  name,
+}) => {
+  console.log("ModalWithForm");
 
-    console.log("ModalWithForm");
+  return (
+    <div className={`modal modal_type_${name}`}>
+      <div className='modal__content'>
+        <button className='modal__close1' type='button' onClick={onClose}>
+          <img src={closeIcon}></img>
+        </button>
 
-    return (
-        <div className={`modal modal_type_${name}`}>
-            <div className="modal__content">
-                <button className="modal__close1" type="button" onClick={onClose}>
-                Close
-                </button>
-                
-                <h3 className="modal__title"> {title}</h3>
-                <form className="modal__form">{children}</form>
-                 <button type="submit"> {buttonText}</button>
-           </div> 
-        </div>
-    );
+        <h3 className='modal__title'> {title}</h3>
+        <form className='modal__form'>{children}</form>
+        <button type='submit'> {buttonText}</button>
+      </div>
+    </div>
+  );
 };
 
 export default ModalWithForm;
