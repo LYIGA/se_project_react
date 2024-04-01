b5452a958edecf5b3e675ffbd0c920b3f335cc95
SmithAkin <s29smit@gmail.com>
2024-03-31 22:22:41 -0500

    change to project 10

--- src/ToggleSwitch/ToggleSwitch.js
+++ src/ToggleSwitch/ToggleSwitch.js
@@ -1,11 +1,22 @@
-import React from"react";
+import React, { useState } from"react";
 import './ToggleSwitch.css'
 
 const ToggleSwitch = () => {
-console.log("Toggle")
+    const [currentTemperatureUnit, handleToggleSwitch] = useState("C")
+
+const handleChange = (e) => {
+if(currentTemperatureUnit === 'C') handleToggleSwitchChange('F')
+if(currentTemperatureUnit === 'F') handleToggleSwitchChange('C')
+
+}
     return (
 <label className="switch">
-    <input type='checkbox' className="switch__box"/>
+    <input type='checkbox'
+     className="switch__box"
+     onChange={handleChange}/>
+     <span className={currentTemperatureUnit === 'F' ? "switch__slider switch__slider-F": "switch__sliderswitch__slider-C"}></span>
+     <p className={`switch__temp-F ${currentTemperatureUnit=== 'F' && "switch__active"}`}>F</p>
+     <p className={`switch__temp-C ${currentTemperatureUnit=== 'C' && "switch__active"}`}>F</p>
 </label>
     )
 }
