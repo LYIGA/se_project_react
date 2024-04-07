d90cee5197b4250fe06e5bc16134607dc87da313
SmithAkin <s29smit@gmail.com>
2024-04-07 13:31:35 -0500

    update to project 11

--- src/ToggleSwitch/ToggleSwitch.js
+++ src/ToggleSwitch/ToggleSwitch.js
@@ -1,24 +1,26 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import './ToggleSwitch.css'
+import { CurrentTemperatureUnitContext } from "../contexts/CurrentTemperatureUnitContext";
 
 const ToggleSwitch = () => {
-    const [currentTemperatureUnit, handleToggleSwitchChange] = useState("C")
+    // const [currentTemperatureUnit, handleToggleSwitchChange] = useState("C")
 
-    const handleChange = (e) => {
-        if (currentTemperatureUnit === 'C') handleToggleSwitchChange('F')
-        if (currentTemperatureUnit === 'F') handleToggleSwitchChange('C')
+    // const handleChange = (e) => {
+    //     if (currentTemperatureUnit === 'C') handleToggleSwitchChange('F')
+    //     if (currentTemperatureUnit === 'F') handleToggleSwitchChange('C')
 
+    const {currentTemperatureUnit, handleToggleSwitchChenge} = useContext(CurrentTemperatureUnitContext)
     }
     return (
         <label className="switch">
             <input type='checkbox'
                 className="switch__box"
-                onChange={handleChange} />
+                onChange={handleToggleSwitchChenge} />
             <span className="switch__slider"></span>
             <p className={`switch__temp-F ${currentTemperatureUnit === 'F' && "switch__active"}`}>F</p>
             <p className={`switch__temp-C ${currentTemperatureUnit === 'C' && "switch__active"}`}>C</p>
         </label>
     )
-}
+
 
 export default ToggleSwitch
