f806102fcc2e794a30a19e620a454c922e8dfb46
SmithAkin <s29smit@gmail.com>
2024-04-10 16:30:00 -0500

    update to project 10

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,40 +1,41 @@
-import {defaultClothingItems} from "../util/constants";
+import { defaultClothingItems } from "../util/constants";
 import WeatherCard from "../../WeatherCard/weatherCard";
 import ItemCard from "../ItemCard/ItemCard";
 import { useMemo, useContext } from "react";
-import {CurrentTemperatureUnitContext} from "../../contexts/CurrentTemperatureUnitContext"
+import { CurrentTemperatureUnitContext } from "../../contexts/CurrentTemperatureUnitContext";
 
-function Main({weatherTemp, onselectCard}) {
-
-  const {currentTemperatureUnit } = useContext(CurrentTemperatureUnitContext)
-  const weatherType =useMemo(() => {
-  const temp = weatherTemp?.temperature?.[currentTemperatureUnit] || 999
+function Main({ weatherTemp, onselectCard }) {
+  console.log(weatherTemp);
+  const { currentTemperatureUnit } = useContext(CurrentTemperatureUnitContext);
+  console.log(currentTemperatureUnit);
+  const temp = weatherTemp?.temperature?.[currentTemperatureUnit] || 999;
+  const weatherType = useMemo(() => {
     if (weatherTemp >= 86) {
-      return 'hot';
+      return "hot";
     } else if (temp >= 66 && temp <= 85) {
-      return 'warm';
+      return "warm";
     } else if (temp <= 65) {
-      return 'cold';
+      return "cold";
     }
-  },[weatherTemp]);
+  }, [weatherTemp]);
 
   const filteredCards = defaultClothingItems.filter((item) => {
-    return item.weather.toLowerCase() === weatherType
+    return item.weather.toLowerCase() === weatherType;
   });
-  
-
-    return <main className='main'>
-      <WeatherCard day={false} type='storm' weatherTemp={weatherTemp}/>
+console.log(weatherTemp, temp)
+  return (
+    <main className='main'>
+      <WeatherCard day={false} type='storm' weatherTemp={temp} />
       <section className='card_section' id='card section'>
-        Today is {weatherTemp} / You may want to wear:
+        Today is {temp} / You may want to wear:
         <div className='card_items'>
           {filteredCards.map((item, index) => (
-            <ItemCard item={item} key={index} onselectCard={onselectCard}/>
-
+            <ItemCard item={item} key={index} onselectCard={onselectCard} />
           ))}
         </div>
       </section>
-    </main>;
+    </main>
+  );
 }
 
-export default Main
+export default Main;
